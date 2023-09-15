8e9d74cf69389bf9d2c8bb28f9b2fa471a58c2b8
diyooor <diyorsattarov7@gmail.com>
2023-09-15 15:29:41 -0500

    QApp QWidget (black background) window 320x240

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <QtWidgets>
 
-int main() {
-	std::cout << "helloworld" << std::endl;
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QWidget window;
+	window.setStyleSheet("background-color: black");
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(
+        QApplication::translate("toplevel", "Top-level widget"));
+    return app.exec();
 }
